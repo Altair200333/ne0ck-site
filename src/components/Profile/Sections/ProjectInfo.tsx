9d3957b44ef002783e7cbdcacf24b99c1f6d37fc
Mike Petrenko <petrenko-m-p@mail.ru>
2025-07-20 00:07:48 +0700

    fix: add interactive card UI

--- src/components/Profile/Sections/ProjectInfo.tsx
+++ src/components/Profile/Sections/ProjectInfo.tsx
@@ -11,7 +11,13 @@ const ProjectInfo: React.FC<{
     <Stack
       onClick={onClick}
       gap={1}
-      _hover={{ backgroundColor: "#424461" }}
+      _hover={{
+        backgroundColor: "#424461",
+        backgroundImage:
+          "radial-gradient(circle at center, rgba(49, 130, 206, 0.04) 0%, rgba(66, 68, 97, 0) 80%)",
+        boxShadow:
+          "inset 0 0 0 2px rgba(49, 130, 206, 0.6), 0 0 4px rgba(49, 130, 206, 0.2)",
+      }}
       p={4}
       borderRadius={"12px"}
       transition={"0.2s ease-in-out"}
